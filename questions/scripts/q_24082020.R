deea491022bc142a18ee980a9f60c3dbc2fc262f
mohit2152sharma <mohitlakshya@gmail.com>
2020-08-25 01:24:26 +0530

    plot changes

--- questions/scripts/q_24082020.R
+++ questions/scripts/q_24082020.R
@@ -2,27 +2,31 @@ library(ggplot2)
 library(patchwork)
 
 plot_fn = function(df, option){
+  if(is.infinite(max(df$values))) ymax = 3 else ymax = max(df$values)
+  
   ggplot(df, aes(x=x, y=values))+
     geom_line() +
     labs(title=paste0('Option ', option), x='F', y='Density') +
     theme_classic() +
-    geom_vline(aes(xintercept = 0), color = 'red', linetype = 'longdash')
+    geom_vline(aes(xintercept = 0), color = 'red', linetype = 'longdash') +
+    scale_x_continuous(limits = c(min(df$x), max(df$x)), expand = c(0,0)) +
+    scale_y_continuous(limits = c(min(df$values), ymax), expand = c(0,0))
 }
 
 norm_dist = data.frame('x' = seq(-10, 10, 0.01),
                        'values' = dnorm(seq(-10, 10, 0.01)))
 opt_a = plot_fn(norm_dist, 'A') 
 
-f_dist_b = data.frame('x' = seq(0, 10, 0.01),
-                       'values' = df(seq(0,10,0.01), 1,1))
+f_dist_b = data.frame('x' = seq(-10, 10, 0.01),
+                       'values' = df(seq(-10,10,0.01), 1,1))
 opt_b = plot_fn(f_dist_b, 'B')
 
 f_dist_c = data.frame('x' = seq(-10, 10, 0.01),
                     'values' = df(seq(-10, 10, 0.01), 5, 10))
 opt_c = plot_fn(f_dist_c, 'C')
 
-f_dist_d = data.frame('x' = seq(0,10,0.01),
-                        'values' = df(seq(0,10,0.01), 100,100))
+f_dist_d = data.frame('x' = seq(-10,10,0.01),
+                        'values' = df(seq(-10,10,0.01), 100, 100))
 opt_d = plot_fn(f_dist_d, 'D')
 opt_d
 
